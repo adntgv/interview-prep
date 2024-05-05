d50012cd2eff7b5d9f7e889dc755ec613e2b1406
Aidyn Torgayev <a.torgayev@arbuz.kz>
2024-05-05 12:00:25 +0500

    reverse linked list

---
+++ algo/lists/reverseLinkedList_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "five",
+			args: args{
+				head: LinkedListFromArgs(1, 2, 3, 4, 5),
+			},
+			want: LinkedListFromArgs(5, 4, 3, 2, 1),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
